e0f1c5ee2300d53626eaa316f77c841cfb83039a
Andrey Svyrskyi <akkad1v@gmail.com>
2024-05-06 03:09:09 +0300

    'update'

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 
-export const params = {
+interface Params {
+  query: string;
+  client_id: string;
+  url: string;
+  orientation: string;
+  page: number;
+  per_page: number;
+}
+
+export const params: Params = {
   query: "",
   client_id: "35YdqMFUVYORJ1_Z6Ti-nFesei5CNrXoMt15-kOzhj8",
   url: "https://api.unsplash.com/search/photos",
@@ -14,7 +23,10 @@ export const params = {
 //   return data;
 // };
 
-export const requestPhotosByQuery = async (query = "", page) => {
+export const requestPhotosByQuery: (
+  query: string,
+  page: number
+) => Promise<any> = async (query = "", page) => {
   const updatedParams = { ...params, query, page };
   const { data } = await axios.get(updatedParams.url, {
     params: updatedParams,
